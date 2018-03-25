6fd66ddbce80dd11f081673c83c5511653341424
John Elm <gitkraken.com@johnelm.com>
2018-03-24 20:45:55 -0700

    moved cli to separate file, added tests

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+#!/usr/bin/env node
 const nodeJxa = require('./node-jxa');
+const DEBUG_SWITCHES = [ '--debug', '-d' ];
+const nonProcessArgs = process.argv.slice( 2 );
+const nonDebugSwitchArgs = nonProcessArgs.filter( arg => !DEBUG_SWITCHES.includes( arg ));
+const jxaScriptArg = nonDebugSwitchArgs[ 0 ];
 
-if ( process.argv.length < 3 ) {
+if ( !nonDebugSwitchArgs.length ) {
   console.error( 'error: no jxa script specified' );
-  process.exit( 1 );
+} else {
+  nodeJxa(
+    jxaScriptArg,
+    {
+      debug: 
+        ['true', '1'].includes ( process.env.NODE_DEBUG_JXA ) 
+        ||
+        !!(nonProcessArgs.filter( arg => DEBUG_SWITCHES.includes( arg )).length)
+    }
+  )
 }
-
-nodeJxa(
-  process.argv[ 2 ],
-  {
-    debug: ['true', '1'].includes ( process.env.NODE_DEBUG_JXA ) 
-    || 
-    process.argv[ 1 ].includes( 'debug' )
-  }
-)
-
--- index.spec.js
+++ index.spec.js
@@ -1,98 +1,79 @@
-const browserify = require('browserify');
-const cp = require( 'child_process' );
-const clearRequire = require( 'clear-require' );
+const nodeJxa = require('./node-jxa');
 
-jest.mock('child_process');
-jest.mock('browserify');
+jest.mock('./node-jxa');
 
-const nodeJxa = require( './node-jxa' );
-
-
-beforeEach( () => {
+afterEach(() => {
+  // delete require.cache[require.resolve("./index")];
   // jest.resetModules();
-  jest.resetAllMocks();
-    // delete require.cache[require.resolve("./index")];
 })
 
-  test( 'adds stdlib to top of script', () => {})
-  test( 'adds exit to bottom of script', () => {})
-  test( 'adds debug option given env var', () => {})
-  test( 'adds debug option based on executed script', () => {})
-  test( 'omits debug option properly, absent env var and debug script', () => {})
-  test( '', () => {})
-  test( '', () => {})
+beforeEach(() => {
+  // jest.resetModules();
+  // jest.resetAllMocks();
+  delete require.cache[require.resolve("./index")];
+})
+describe('debug mode is properly enabled', () => {
 
-test('adds top line', () => {
-  let b = {
-    add: jest.fn(),
-    bundle: jest.fn()
-  }
-  let cpMock = {
-    exec: jest.fn()
-  }
 
-  let src = 'SRC';
+  test('adds debug option given NODE_DEBUG_JXA=1 env var', () => {
+    process.env.NODE_DEBUG_JXA = '1';
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
+  })
+  test('adds debug option given NODE_DEBUG_JXA=1 env var', () => {
+    process.env.NODE_DEBUG_JXA = 'true';
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
+  })
 
-  cp.exec.mockReturnValue(cpMock);
-  browserify.mockReturnValue(b)
 
-  process.argv = [ 'node', 'index', './example' ]; // we don't care about the first two
+  test('adds debug option based on -d switch', () => {
+    process.argv = ['node', 'index', './example.js', '-d']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
+  })
+  test('adds debug option based on --debug switch', () => {
+    process.argv = ['node', 'index', './example.js', '--debug']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
+  })
+  test('adds debug option with switch first in args', () => {
+    process.argv = ['node', 'index', '--debug', './example.js']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
+  })
+  test('omits debug option properly, absent env var and debug script', () => {
+    process.argv = ['node', 'index', './example.js']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', false);
+  })
+})
 
-  require( './index' );
-  
-  expect( b.add.mock.calls[0][0]).toBe( './example' );
+describe('handles jxa script argument properly', () => {
+  test('passes jxa script name', () => {
+    process.argv = ['node', 'index', './example.js']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
+  });
+   test('passes jxa script name, with debug switch', () => {
+    process.argv = ['node', 'index', './example.js', '-d']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
+  });
+   test('passes jxa script name, with debug switch first', () => {
+    process.argv = ['node', 'index', '-d', './example.js']; // we don't care about the first two
+    require('./index');
+    expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
+  });
+   test('handles missing script argument', () => {
+    process.argv = ['node', 'index']; // missing script arg
+    require('./index');
+    expect(nodeJxa.mock.calls.length).toBe(0);
+  });
+   test('handles missing script argument, with debug switch', () => {
+    process.argv = ['node', 'index', '--debug']; // missing script arg
+    require('./index');
+    expect(nodeJxa.mock.calls.length).toBe(0);
+  });
 
-  // expect( b.bundle.mock.calls[0][1]).toBe( 'SRC' );
-  // expect( cpMock.exec.mock.calls[0][0]).toBe( 'osascript -l JavaScript' );
-  // expect( b.bundle.mock.calls[0][0]).toBe( '')
-  
 });
-
-test( 'handles missing script argument', () => {
-
-  process.argv = [ 'node', 'index' ]; // missing script arg
-
-  let b = {
-    add: jest.fn(),
-    bundle: jest.fn()
-  }
-  browserify.mockReturnValue(b);
-
-  require( './index' );
-
-  expect(b.add.mock.calls.length).toBe(0);  
-
-})
-test( 'handles debug argument to enable sourcemaps', () => {
-
-  clearRequire('./index')
-
-  let b = {
-    add: jest.fn(),
-    bundle: jest.fn()
-  }
-  let cpMock = {
-    exec: jest.fn()
-  }
-
-  browserify.mockReturnValue(b);
-  cp.exec.mockReturnValue(cpMock);
-  
-  process.argv = [ 'node', 'index', './example.js', '--debug' ];
-
-  require( './index' );
-
-
-  expect( b.add.mock.calls[0][1]).toHaveProperty( 'debug', true );
-  expect( b.add.mock.calls[0][0]).toBe( './example.js' );
-
-
-})
-
-// test( 'runs osascript command', () => {
-
-//   process.argv = [ 'node', 'index', './example' ]; // we don't care about the first two
-
-//   require( './index' );
-
-// });
