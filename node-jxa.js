92d42e3269b0c2ed875e74c2c65b722b2d3b00ff
John Elm <gitkraken.com@johnelm.com>
2018-03-26 14:39:12 -0700

    fixed issue with moching browserify() outside module.exports

--- node-jxa.js
+++ node-jxa.js
@@ -5,10 +5,10 @@ const HEAD = 'window = this;\nObjC.import("stdlib");\n';
 const TAIL = ';\n$.exit(0);';
 const OSA_JXA_CMD = 'osascript -l JavaScript';
 
-const b = browserify();
 
 module.exports = ( scriptFile, browserifyOptions = {} ) => {
-  b.add( process.argv[ 2 ], browserifyOptions );
+  const b = browserify();
+  b.add( scriptFile, browserifyOptions );
   b.bundle( ( error, src ) => {
     if ( !!error ) {
       console.error( error );
@@ -25,8 +25,10 @@ module.exports = ( scriptFile, browserifyOptions = {} ) => {
         console.error( error );
         process.exit( 1 );
       }
-      stdout.pipe(process.stdout);
-      stderr.pipe(process.stderr);
+      console.log( stdout );
+      console.error( stderr );
+      // stdout.pipe(process.stdout);
+      // stderr.pipe(process.stderr);
     });
     process.stdin.write( fixed );
     process.stdin.end();
