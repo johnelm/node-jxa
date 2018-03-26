672e58a970bbe9e597dd200c08ca99e4be935f0c
John Elm <gitkraken.com@johnelm.com>
2018-03-26 14:37:54 -0700

    fixed module issues testing env vars and argv

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 const nodeJxa = require('./node-jxa');
 const DEBUG_SWITCHES = [ '--debug', '-d' ];
-const nonProcessArgs = process.argv.slice( 2 );
-const nonDebugSwitchArgs = nonProcessArgs.filter( arg => !DEBUG_SWITCHES.includes( arg ));
-const jxaScriptArg = nonDebugSwitchArgs[ 0 ];
 
-if ( !nonDebugSwitchArgs.length ) {
-  console.error( 'error: no jxa script specified' );
-} else {
-  nodeJxa(
-    jxaScriptArg,
-    {
-      debug: 
-        ['true', '1'].includes ( process.env.NODE_DEBUG_JXA ) 
-        ||
-        !!(nonProcessArgs.filter( arg => DEBUG_SWITCHES.includes( arg )).length)
-    }
-  )
-}
+module.exports = ( processArgs ) => {
+
+  const nonProcessArgs = processArgs.argv.slice( 2 );
+  const nonDebugSwitchArgs = nonProcessArgs.filter( arg => !DEBUG_SWITCHES.includes( arg ));
+  const jxaScriptArg = nonDebugSwitchArgs[ 0 ];
+
+  if ( !nonDebugSwitchArgs.length ) {
+    console.error( 'error: no jxa script specified' );
+  } else {
+    nodeJxa(
+      jxaScriptArg,
+      {
+        debug: 
+          ['true', '1'].includes ( processArgs.env.NODE_DEBUG_JXA ) 
+          ||
+          !!(nonProcessArgs.filter( arg => DEBUG_SWITCHES.includes( arg )).length)
+      }
+    )
+  }
+};
+
+module.exports( { argv: process.argv, env: process.env } );
--- index.spec.js
+++ index.spec.js
@@ -1,78 +1,115 @@
 const nodeJxa = require('./node-jxa');
-
 jest.mock('./node-jxa');
 
-afterEach(() => {
-  // delete require.cache[require.resolve("./index")];
-  // jest.resetModules();
-})
+const index = require('./index');
+
+const defaultProcessArgs = {
+  env: {},
+  argv: ['node', 'index', 'example.js']
+};
 
 beforeEach(() => {
-  // jest.resetModules();
-  // jest.resetAllMocks();
-  delete require.cache[require.resolve("./index")];
+  jest.resetAllMocks();
 })
+
+
 describe('debug mode is properly enabled', () => {
-
-
   test('adds debug option given NODE_DEBUG_JXA=1 env var', () => {
-    process.env.NODE_DEBUG_JXA = '1';
-    require('./index');
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        env: {
+          NODE_DEBUG_JXA: '1'
+        }
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
   })
-  test('adds debug option given NODE_DEBUG_JXA=1 env var', () => {
-    process.env.NODE_DEBUG_JXA = 'true';
-    require('./index');
+  test('adds debug option given NODE_DEBUG_JXA=true env var', () => {
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        env: {
+          NODE_DEBUG_JXA: 'true'
+        }
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
   })
-
 
   test('adds debug option based on -d switch', () => {
-    process.argv = ['node', 'index', './example.js', '-d']; // we don't care about the first two
-    require('./index');
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', './example.js', '-d']
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
   })
   test('adds debug option based on --debug switch', () => {
-    process.argv = ['node', 'index', './example.js', '--debug']; // we don't care about the first two
-    require('./index');
+
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', './example.js', '--debug']
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
   })
   test('adds debug option with switch first in args', () => {
-    process.argv = ['node', 'index', '--debug', './example.js']; // we don't care about the first two
-    require('./index');
+
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', '--debug', './example.js']
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', true);
   })
   test('omits debug option properly, absent env var and debug script', () => {
-    process.argv = ['node', 'index', './example.js']; // we don't care about the first two
-    require('./index');
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', './example.js']
+      })
+    );
     expect(nodeJxa.mock.calls[0][1]).toHaveProperty('debug', false);
   })
 })
 
 describe('handles jxa script argument properly', () => {
   test('passes jxa script name', () => {
-    process.argv = ['node', 'index', './example.js']; // we don't care about the first two
-    require('./index');
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', './example.js']
+      })
+    );
     expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
   });
-   test('passes jxa script name, with debug switch', () => {
-    process.argv = ['node', 'index', './example.js', '-d']; // we don't care about the first two
-    require('./index');
+  test('passes jxa script name, with debug switch', () => {
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', './example.js', '-d']
+      })
+    );
     expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
   });
-   test('passes jxa script name, with debug switch first', () => {
-    process.argv = ['node', 'index', '-d', './example.js']; // we don't care about the first two
-    require('./index');
+  test('passes jxa script name, with debug switch first', () => {
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', '-d', './example.js']
+      })
+    );
     expect(nodeJxa.mock.calls[0][0]).toBe('./example.js');
   });
-   test('handles missing script argument', () => {
-    process.argv = ['node', 'index']; // missing script arg
-    require('./index');
+  test('handles missing script argument', () => {
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index']
+      })
+    );
     expect(nodeJxa.mock.calls.length).toBe(0);
   });
-   test('handles missing script argument, with debug switch', () => {
-    process.argv = ['node', 'index', '--debug']; // missing script arg
-    require('./index');
+  test('handles missing script argument, with debug switch', () => {
+    index(
+      Object.assign({}, defaultProcessArgs, {
+        argv: ['node', 'index', '--debug']
+      })
+    );
     expect(nodeJxa.mock.calls.length).toBe(0);
   });
 
