36063907f2c8b0c3eebbee2dc8f43b8ebc09b749
John Elm <gitkraken.com@johnelm.com>
2018-04-03 18:01:45 -0700

    A better example.js JXA script
    closes #6

--- example.js
+++ example.js
@@ -1,9 +1,21 @@
-ObjC.import( "stdlib" );
+#! /usr/bin/env node-jxa
 
-const outlookApp = Application( "Microsoft Outlook" );
+// say we need lodash for our script  ( remember to `yarn` or `npm install` before running this example! )
+const _ = require( 'lodash' ); 
 
-const _ = require('lodash');
+const app = Application.currentApplication(); // or Application( "Microsoft Outlook" ), or whatever scriptable application you desire
+app.includeStandardAdditions = true
 
-outlookApp.includeStandardAdditions = true
+console.log( Date(), 'about to show a notification!' );
 
-outlookApp.displayAlert('hello!');
+let input = 'My,^%)awesome,. @*`JXA".%script. (&)&is,! * running!';  // this ugly string needs cleaning up
+
+// show an OSX notification
+// DND must be off, and the current Application allowed to show notifications in OS X preferences
+app.displayNotification( "notification details go here", {
+  withTitle: "An OS X notification courtesy of JXA",
+  subtitle: _.join( _.words( input ), ' ' ) + '!',  // let's use lodash to clean up the input string
+  soundName: "Frog"
+})
+
+console.log( Date(), 'done showing the notification' );
